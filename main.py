83bb6528149433dc0806a7789afb366eb8fe2268
Jeroth Diggeden <jeroth.diggeden@protonmail.com>
2024-08-13 17:32:53 +0800

    Team Selection

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ from bs4 import BeautifulSoup
 st.set_page_config(page_title="NUFC Web App", page_icon=":material/edit:", layout="wide",
 				   initial_sidebar_state="expanded")
 
-team_id_lst = ['33', '34', '35', '36', '37', '38', '39', '40', '41', '42',
-		   '43', '44', '45', '46', '47', '48', '49', '50', '51', '52']
+# team_id_lst = ['33', '34', '35', '36', '37', '38', '39', '40', '41', '42',
+# 		   '43', '44', '45', '46', '47', '48', '49', '50', '51', '52']
+
+team_id_lst = {'33': '', '34': '', '35': '', '36': '', '37': '', '38': '', '39': '', '40': '', '41': '', '42': '',
+		   '43': '', '44': '', '45': '', '46': '', '47': '', '48': '', '49': '', '50': '', '51': '', '52': '', '53': ''}
 
 st.sidebar.title("EPL Stats")
 select_team = st.selectbox('Team', team_id_lst)
